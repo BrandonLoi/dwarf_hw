3659e440a27f4a73f7db98d7732d1a4e0928ca12
Brandon Loi <brandon.loi@gmail.com>
2017-06-14 10:16:46 -0400

    Add textbox for comments

--- src/Comments.js
+++ src/Comments.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
 
 class Comments extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      comment: ''
+    }
+    this.updateComment = this.updateComment.bind(this)
+  }
+
+  updateComment(ev) {
+    this.setState({
+      comment: ev.target.value
+    })
+  }
+
   render() {
     return (
       <div className="comments">
-        <textarea>
-
+        <textarea value={this.state.comment} onChange={this.updateComment} placeholder='Place Comment Here'>
         </textarea>
         <button className="button">Submit Comment</button>
       </div>
